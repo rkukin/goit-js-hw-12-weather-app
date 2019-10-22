bdccc2dd9e838867b3d0a2b9f79ee1517340ff9e
rkukin <romakukin@gmail.com>
2019-10-22 20:32:07 +0300

    small fixes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+'use strict';
+
 import './styles.css';
+import './getGeoPosition.js';
+import './fetchWeather.js';
+
+// import PNotify from 'node_modules/pnotify/dist/es/PNotify.js';
+// import PNotifyButtons from 'node_modules/pnotify/dist/es/PNotifyButtons.js';
