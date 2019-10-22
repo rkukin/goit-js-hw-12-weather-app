b2b5d981ecbac22de4ec82b532b60d0e02455f45
rkukin <romakukin@gmail.com>
2019-10-22 21:09:17 +0300

    geolocation WIP

--- src/getGeoPosition.js
+++ src/getGeoPosition.js
@@ -1,5 +1,3 @@
 'use strict';
 
-export default function getGeoPosition() {
-  navigator.geolocation();
-}
+export default function getGeoPosition() {}
--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ import './fetchWeather.js';
 
 // import PNotify from 'node_modules/pnotify/dist/es/PNotify.js';
 // import PNotifyButtons from 'node_modules/pnotify/dist/es/PNotifyButtons.js';
+
+window.onload = function() {
+  let geoSuccess = function(position) {
+    let startPos = position;
+    console.log(startPos.coords);
+    return startPos.coords;
+  };
+  let geoError = function() {
+    alert('Please use input');
+  };
+
+  const options = {
+    maximumAge: 1800000,
+    enableHighAccuracy: true,
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, options);
+};
