3f0cb20ac404a3916a4f7acd48825d034b035e7c
rkukin <romakukin@gmail.com>
2019-10-23 15:56:57 +0300

    'wip'

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -5,17 +5,21 @@ export default function (coordinates) {
   // Location Name	query = New York	The standard way of passing a location name to the API.
   // Coordinates (Lat/Lon)	query = 40.7831,-73.9712
 
-  const ACCESS_KEY = '02a147ec3a44e145b334fac4a63397f3\n';
-  fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates},`
-  .then(response=>{
-    if (response.ok) return response.json();
+  const ACCESS_KEY = '02a147ec3a44e145b334fac4a63397f3';
 
-    throw new Error(`Error while fetching: ${response.statusText}`);
+  // return fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates}`)
+
+  return new Promise((resolve, reject) => {
+    fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates}`).then(response => {
+      console.log();
+      response.json().then(resp => {
+        if (!resp.error)
+        resolve(resp);
+        else {
+          reject(resp.error.info);
+        }
+
+      })
+    });
   })
-    .then(data=> {
-      console.log(data)
-    })
-    .catch(error => {
-      console.error(error)
-    }))
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,31 @@ import fetchWeather from "./fetchWeather";
 
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
 
+const weatherSection = document.querySelector('#weather');
+const cityForm = document.querySelector('#search-form');
+const cityName = document.querySelector('[name="city"]');
+
+const locationValue = document.querySelector('[data-field=location]');
+const tempValue = document.querySelector('[data-field=temp]');
+const humidityValue = document.querySelector('[data-field=humidity]');
+const windValue = document.querySelector('[data-field=wind]');
+const conditionsValue = document.querySelector('[data-field=conditions]');
+
+// console.log(locationValue.innerHTML);
+
 getGeoPosition()
   .then(position => {
-  PNotify.alert(`${position.longitude}, ${position.latitude}`)
-    // fetchWeather(position.longitude, position.latitude)
+    fetchWeather(` ${position.latitude}, ${position.longitude}`)
+      .then(data => {
+        console.log(data);
+      })
 })
   .catch(error=> PNotify.alert(error));
 
+cityForm.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  fetchWeather(cityName.value).then(data => {
+      console.log(data)
+  }).catch(error => PNotify.alert(error)
+  );
+});
